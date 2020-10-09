e148d2183543f898bdbf7d6bd1672c8f8ca30257
unknown <swethagottimukkala@gmail.com>
2020-10-09 16:46:12 +0530

    models and db added

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: "mongodb+srv://Swetha41:Swetha@41@cluster0.dmhzb.mongodb.net/nodePassport?retryWrites=true&w=majority"
+}
--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
 
+//DB config
+const db = require("./config/keys").MongoURI;
+
+//connect to Mongo
+mongoose.connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => console.log("MongDb Connected"))
+.catch(err => console.log(err));
 //EJS
 app.use(expressLayouts);
 app.set("view engine", "ejs");
