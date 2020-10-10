2c47f56bd280ee817e61eda8089708e1b0931320
unknown <swethagottimukkala@gmail.com>
2020-10-10 12:15:03 +0530

    partials added

--- routes/users.js
+++ routes/users.js
@@ -18,17 +18,30 @@ router.post("/register", (req, res) => {
     
     //check for all the fields filled
     if(!name || !email || !password || !password2){
-        errros.push({ msg: "Please fill in all the fields" })
+        errros.push({ msg: "Please fill in all the fields" });
     }
 
     //passwords match
     if(password !== password2){
-        errros.push({ msg: "Password and confirm password should be same" })
+        errros.push({ msg: "Password and confirm password should be same" });
     }
 
     //check password length
-    if(password.length > 6){
-        errros.push({ msg: "password length should be more than 6 characters"})
+    if(password.length < 6){
+        errros.push({ msg: "password length should be atleast 6 characters"});
+    }
+
+    if(errros.length > 0){
+        res.render("register", {
+            errros,
+            name,
+            email,
+            password,
+            password2
+        });
+    }
+    else{
+        res.send("pass");
     }
 
 });
